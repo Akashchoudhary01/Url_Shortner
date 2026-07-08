5c480a3f567bc81d1034ccd5c81caf58b93740a0
Akash Choudhary <akashkrchoudhary007@gmail.com>
2026-07-09 00:07:31 +0530

    /

--- src/Controllers/Url.controller.ts
+++ src/Controllers/Url.controller.ts
@@ -1,4 +1,22 @@
-const handleUrlGeneration = async(req , res)=>{
+import { Request , Response } from "express";
+import { nanoid } from "nanoid";
+import URL from "../Models/Url.models.js";
+const handleUrlGeneration = async(req:Request , res:Response)=>{
+
+    const shortId = nanoid(8);
+    const {url} = req.body;
+    if(!url){
+        return res.status(400).json({error : "URL is Required"})
+    }
+    URL.create({
+        shortID : shortId,
+        redirectUrl : url,
+        visitHistory : []
+
+    })
+    return res.json({id : shortId});
+
+
 
 }
 export {handleUrlGeneration};
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 const app = express();
+import urlRoute from './Routes/Url.route.js';
 
 app.use(express());
 
@@ -10,6 +11,7 @@ app.get("/" , (req , res)=>{
     res.send("Hello Jai Shree Ram !")
 });
 
+app.use("/url" , urlRoute );
 app.listen(PORT , ()=>{
     console.log( `App is live at http://localhost:${PORT}`)
 })
