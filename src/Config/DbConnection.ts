5ee148dbb9866f78254bc7b47c9704753b106ee2
Akash Choudhary <akashkrchoudhary007@gmail.com>
2026-07-08 23:53:32 +0530

    ./

--- src/Config/DbConnection.ts
+++ src/Config/DbConnection.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
-const URL:string = 'mongodb://127.0.0.1:27017/URL_SHORTNER'
-export const dbConnection = async()=>{
+const URL:string = "";
+export const dbConnection = async(URL:string)=>{
     await mongoose.connect(URL!)
     .then(()=> console.log(`mongo Db connected`))
     .catch(()=> console.log("Something went wrong in DB Connection"))
